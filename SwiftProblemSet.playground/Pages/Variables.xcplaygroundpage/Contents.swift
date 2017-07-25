5fde2395b2ad64c5fae001769adca78eefb22676
Curtis Bridges <curtis@curtisbridges.com>
2017-07-25 19:28:40 -0400

    fixes required by code review #2

--- SwiftProblemSet.playground/Pages/Variables.xcplaygroundpage/Contents.swift
+++ SwiftProblemSet.playground/Pages/Variables.xcplaygroundpage/Contents.swift
@@ -15,7 +15,7 @@
  */
 var gallonsRemainingInTank: Double
 var ageOfSpecimenInWeeks: Int
-var birthplace: String
+let birthplace: String
 let myFirstProgrammingLanguage = "BASIC"
 //: ### Exercise 2
 //: Make the minimal changes necessary to fix the following compiler errors related to assignment, variables, and constants.
@@ -26,17 +26,17 @@ let bestProgrammingLanguage = myFirstProgrammingLanguage
 //: ### Exercise 3
 //: Below is some code to reverse a string. We've declared `stringToReverse` and `arrayOfCharacters` as variables. Rewrite this code to reverse a string using only constants declared with `let`, no variables. **Note**: In the new version you may use as many constants as you'd like.
 let stringToReverse = "Mutable or Immutable? That is the question."
-let reveredString = stringToReverse.characters.reversed()
-/*: 
-### Exercise 4
+let reveredString = String(stringToReverse.characters.reversed())
+/*:
+ ### Exercise 4
 
  For the following descriptions, create a variable or constant that best represents it:
 
-- an `Int` value representing the hunger level of an animal over time.
-- an `Int` value representing the score always required to win a game.
-- a `String` value representing a student’s birthday.
-- a `String` value representing what a user types into the status update area.
-*/
+ - an `Int` value representing the hunger level of an animal over time.
+ - an `Int` value representing the score always required to win a game.
+ - a `String` value representing a student’s birthday.
+ - a `String` value representing what a user types into the status update area.
+ */
 var animalHungerLevel: Int
 let minimumWinningScore = 20000
 let studentBirthday = "19731112"
