f6f2a6701677ac34c191d31b2c96839eeb3b0b5b
Curtis Bridges <curtis@curtisbridges.com>
2017-07-25 18:38:21 -0400

    finish problem set with functions

--- SwiftProblemSet.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
+++ SwiftProblemSet.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -7,14 +7,21 @@ import Foundation
 //: **This exercise is completely optional and is not required for completing the Swift Problem Set.**
 //: ### Challenge 1
 //: Mystery code! What does this code do? Briefly, using comments, describe what is happening in each line. **Hint**: You may need to look up [Int initializers](http://stackoverflow.com/questions/30739460/toint-removed-in-swift-2).
+// declare an array of Strings
 let array = ["A", "13", "B", "5", "87", "t", "41"]
+// declare a sum variable
 var sum = 0
+// for each string in the array
 for string in array {
+    // if the string can be used to construct an integer...
     if Int(string) != nil {
+        // ...construct it...
         let intToAdd = Int(string)!
+        // and add it to the sum
         sum += intToAdd
     }
 }
+// finally, print the sum of all the strings that represent integers in the given string array
 print(sum)
 
 /*:
--- SwiftProblemSet.playground/Pages/Functions.xcplaygroundpage/Contents.swift
+++ SwiftProblemSet.playground/Pages/Functions.xcplaygroundpage/Contents.swift
@@ -5,26 +5,31 @@
 //: ## Functions 
 //: ### Exercise 14
 //: The function `emojiLove` should take two `String` parameters and use them to print a `String` with  the format "stringParameterOne ❤️ stringParameterTwo".
-func emojiLove(/* add parameters here */) {
-    /* add function implementation here */
+func emojiLove(s1: String, s2: String) {
+    print("\(s1) ❤️ \(s2)")
 }
 
-/* Example Function Call
+/* Example Function Call */
  
  emojiLove(s1: "cats", s2: "dogs") // prints "cats ❤️ dogs"
  emojiLove(s1: "udacity", s2: "students") // prints "udacity ❤️ students"
  emojiLove(s1: "peanut butter", s2: "jelly") // prints "peanut butter ❤️ jelly"
  emojiLove(s1: "ying", s2: "yang") // prints "ying ❤️ yang"
  
- */
+
 
 //: ### Exercise 15
 //: The function `median` should take three `Int` parameters and return the `Int` value in the middle.
-func median(/* add parameters here */) /* define the return type */ {
-    /* add function implementation here */
+func median(num1: Int, num2: Int, num3: Int) -> Int {
+    // create an array
+    var values = [num1, num2, num3]
+    // sort it
+    values.sort()
+    // return the middle value
+    return values[1]
 }
 
-/* Example Function Call
+/* Example Function Call */
  
  median(num1: 1, num2: 5, num3: 6) // 5
  median(num1: 2, num2: 1, num3: 4) // 2
@@ -34,7 +39,7 @@ func median(/* add parameters here */) /* define the return type */ {
  median(num1: 2, num2: 3, num3: 1) // 2
  median(num1: 2, num2: 2, num3: 1) // 2
  
- */
+
 
 /*:
  ### Exercise 16
@@ -46,11 +51,16 @@ func median(/* add parameters here */) /* define the return type */ {
  
  **Note**: It is assumed that the input string is given in English.
  */
-func beginsWithVowel(/* add parameters here */) /* define the return type */ {
-    /* add function implementation here */
+func beginsWithVowel(_ value: String) -> Bool {
+    if(value.isEmpty) {
+        return false
+    }
+
+    let character = value.lowercased().characters[value.startIndex]
+    return "aeiou".characters.contains(character)
 }
 
-/* Example Function Call
+/* Example Function Call */
  
  beginsWithVowel("Apples") // true
  beginsWithVowel("pIG") // false
@@ -58,7 +68,7 @@ func beginsWithVowel(/* add parameters here */) /* define the return type */ {
  beginsWithVowel("udacity") // true
  beginsWithVowel("") // false
  
- */
+
 
 /*:
  ### Exercise 17
@@ -71,11 +81,15 @@ func beginsWithVowel(/* add parameters here */) /* define the return type */ {
  
  **Hint**: Re-use the `beginsWithVowel` function.
  */
-func funWithWords(/* add parameters here */) /* define the return type */ {
-    /* add function implementation here */
+func funWithWords(_ string: String) -> String {
+    if beginsWithVowel(string) {
+        return string.uppercased()
+    } else {
+        return string.lowercased()
+    }
 }
 
-/* Example Function Call
+/* Example Function Call */
  
  funWithWords("Apples") // "APPLES"
  funWithWords("pIG") // "pig"
@@ -83,7 +97,7 @@ func funWithWords(/* add parameters here */) /* define the return type */ {
  funWithWords("udacity") // "UDACITY"
  funWithWords("") // ""
  
- */
+
 /*:
  ****
  [Table of Contents](Table%20of%20Contents) | [Previous](@previous) | [Next](@next)
